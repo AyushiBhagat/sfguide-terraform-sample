8ab515c636b9688366512dcffde5221605ee0bf9
AyushiBhagat <ayushi28.92@gmail.com>
2022-06-14 12:05:11 +0530

    Add Database and Warehouse

--- main.tf
+++ main.tf
@@ -24,3 +24,78 @@ resource "snowflake_warehouse" "warehouse" {
 
   auto_suspend = 60
 }
+
+provider "snowflake" { 
+ alias = "security_admin"
+ role  = "SECURITYADMIN"
+ }
+
+
+ resource "snowflake_role" "role" {
+     provider = snowflake.security_admin
+     name     = "TF_DEMO_SVC_ROLE"
+ }
+
+
+ resource "snowflake_database_grant" "grant" {
+     provider          = snowflake.security_admin
+     database_name     = snowflake_database.db.name
+     privilege         = "USAGE"
+     roles             = [snowflake_role.role.name]
+     with_grant_option = false
+ }
+
+
+ resource "snowflake_schema" "schema" {
+     provider   = snowflake.sys_admin
+     database   = snowflake_database.db.name
+     name       = "TF_DEMO"
+     is_managed = false
+ }
+
+
+ resource "snowflake_schema_grant" "grant" {
+     provider          = snowflake.security_admin
+     database_name     = snowflake_database.db.name
+     schema_name       = snowflake_schema.schema.name
+     privilege         = "USAGE"
+     roles             = [snowflake_role.role.name]
+     with_grant_option = false
+ }
+
+ resource "snowflake_schema_grant" "grant" {
+     provider          = snowflake.security_admin
+     database_name     = snowflake_database.db.name
+     schema_name       = snowflake_schema.schema.name
+     privilege         = "CREATE TASK"
+     roles             = [snowflake_role.role.name]
+     with_grant_option = false
+ }
+
+ resource "snowflake_table_grant" "grant" {
+     provider          = snowflake.security_admin
+     database_name     = snowflake_database.db.name
+     schema_name       = snowflake_schema.schema.name
+     privilege         = "SELECT,INSERT"
+     roles             = [snowflake_role.role.name]
+     with_grant_option = false
+     on_future         = true
+     enable_multiple_grants= true
+ }
+
+  resource "snowflake_warehouse_grant" "grant" {
+     provider          = snowflake.security_admin
+     warehouse_name    = snowflake_warehouse.warehouse.name
+     privilege         = "USAGE"
+     roles             = [snowflake_role.role.name]
+     with_grant_option = false
+ }
+
+
+
+
+ resource "snowflake_role_grants" "grants" {
+     provider  = snowflake.security_admin
+     role_name = snowflake_role.role.name
+     roles     = "terraform_test_rl"
+ }
